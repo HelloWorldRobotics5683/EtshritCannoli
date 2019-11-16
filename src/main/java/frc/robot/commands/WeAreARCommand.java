4ebb3fbf1b97493737632459719ca1c609db152f
Katie Markham <>
2019-11-15 16:31:17 -0800

    Update WeAreARCommand.java

--- src/main/java/frc/robot/commands/WeAreARCommand.java
+++ src/main/java/frc/robot/commands/WeAreARCommand.java
@@ -54,6 +54,7 @@ public class WeAreARCommand extends Command {
   // Called once after isFinished returns true
   @Override
   protected void end() {
+    
   }
 
   // Called when another command which requires one or more of the same
